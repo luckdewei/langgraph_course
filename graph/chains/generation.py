8c0bf6dac816d54249990c90251f48df1600d147
weiwenhao <weiwenhao-szgx@bestpay.com.cn>
2026-05-15 10:30:56 +0800

    finish

--- graph/chains/generation.py
+++ graph/chains/generation.py
@@ -1,8 +1,8 @@
-from langchain_classic import hub
+from langsmith import Client
 from langchain_core.output_parsers import StrOutputParser
 from langchain_deepseek import ChatDeepSeek
 
 llm = ChatDeepSeek(model="deepseek-chat", temperature=0)
-prompt = hub.pull("rlm/rag-prompt")
+prompt = Client().pull_prompt("rlm/rag-prompt", dangerously_pull_public_prompt=True)
 
 generation_chain = prompt | llm | StrOutputParser()
--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-def main():
-    print("Hello from langgraph-course!")
+from dotenv import load_dotenv
 
+load_dotenv()
+
+from graph.graph import app
 
 if __name__ == "__main__":
-    main()
+    print("Hello Advanced RAG")
+    print(app.invoke(input={"question": "agent 记忆?"}))
